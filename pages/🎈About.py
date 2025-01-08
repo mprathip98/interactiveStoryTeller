82ea956dd0c88e7479daade4ac69f8331f2c6101
Mohit Prathipati <prathipati.mohit@gmail.com>
2025-01-08 16:50:58 -0700

    🎈About.py

--- pages/🎈About.py
+++ pages/🎈About.py
@@ -2,7 +2,19 @@
 import streamlit as sl
 
 #tab title and page title
-sl.set_page_config(page_title="About")
+sl.set_page_config(page_title="Instructions")
 
 with ((sl.container(border=True))):
-    pass
+    sl.title("About")
+    sl.markdown("It has a total of three pages:")
+    sl.markdown("- Home - All of the main content lies here")
+    sl.markdown("- About - Quick facts about the project")
+    sl.markdown("- Instructions - Simple instructions to use the app.")
+    sl.markdown("This project also has a responsive design, adapting to any screen size")
+
+    sl.title("Documentation")
+    sl.markdown("This project uses Streamlit to cast the website and deploy it through the Streamlit Community Cloud.")
+    sl.markdown("- Design Documentation: https://docs.streamlit.io/")
+    sl.markdown("- Streamlit Community Cloud Deployment Documentation: https://docs.streamlit.io/deploy/streamlit-community-cloud")
+
+
