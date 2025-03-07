1613bb37a741b7c2611b8c804f3837cf54c4ea64
Mohit Prathipati <prathipati.mohit@gmail.com>
2025-03-07 10:26:41 -0700

    🏠Home.py

--- 🏠Home.py
+++ 🏠Home.py
@@ -1,26 +1,10 @@
 #importing library
 import streamlit as sl
 
-page_bg_img = f"""
-<style>
-.st-emotion-cache-bm2z3a {{
-    background-image: url("https://interactivestory.streamlit.app/~/+/media/00b21288c6e019acb0dd9ce88dfd5e15d0e489279c6e33a337097e8d.jpg");
-    background-size: cover;
-}}
 
-.st-emotion-cache-h4xjwg {{
-    background-color: rgba(0, 0, 0, 0);
-}}
-
-.st-emotion-cache-qcpnpn {{
-    background-color: rgba(0, 0, 0, 0.6);
-}}
-</style>
-"""
-sl.markdown(page_bg_img, unsafe_allow_html=True)
 
 #tab title and page title
-#sl.set_page_config(page_title="Home")
+sl.set_page_config(page_title="Home")
 title = sl.title("Interactive Storyteller")
 
 # assigning variables
@@ -1056,4 +1040,7 @@ elif sl.session_state.story_stage == "2b2b1":
 elif sl.session_state.story_stage == "2b2b2":
     endResult()
 
-
+col1, col2 = sl.columns([1,3])
+with col2:
+    #option two
+    sl.image("background.jpg", width=300)
